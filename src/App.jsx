015651e985d0ec466802576542b2ec3f7b0aa798
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-06-24 02:22:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Layout from './Components/Layout'
+
+function App() {
+  
+
+  return (
+    <div className='flex flex-col justify-center w-[100%]' >
+      <Layout/>
+    </div>
+  )
+}
+
+export default App
