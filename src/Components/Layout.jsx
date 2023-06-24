5586c04ad2074d833f3e15ce76b217698e3956c0
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-06-24 17:39:26 +0530

    --refactor--i used class insted of className

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -48,9 +48,9 @@ const Layout = () => {
       <div className="h-[9%] flex justify-between p-2 items-center bg-indigo-300 rounded-t-xl border-b-4">
         <h3 className="text-lg flex">
           Exact Space Chat Group{" "}
-          <span class="relative flex h-3 w-3">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+          <span className="relative flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
           </span>
         </h3>
         <h3>Members:-{getRandomItem(nameList.length)} </h3>
